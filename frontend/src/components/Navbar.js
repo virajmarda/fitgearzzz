c7a549f19e438094168854d27d4ace86406f0fb7
virajmarda <sheetalmarda123@gmail.com>
2026-01-14 14:23:50 +0530

    Fix syntax error in Navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -140,7 +140,7 @@ const Navbar = () => {
                       data-testid="profile-button"
                     >
                       <User className="w-5 h-5 mr-2" />
-                      {user.email || 'Guest;'
+                      {user.email || 'Guest'}
 };
 
 export default Navbar;

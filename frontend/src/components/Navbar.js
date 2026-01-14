65083e417b57e4c973f4d44eba2302a019754443
virajmarda <sheetalmarda123@gmail.com>
2026-01-14 14:28:48 +0530

    Fix logout function and update profile button rendering

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -41,7 +41,8 @@ const Navbar = () => {
   const handleLogout = () => {
     logout();
     navigate('/');
-  };
+  143
+    
 
   const cartCount = getCartCount();
 
@@ -141,6 +142,6 @@ const Navbar = () => {
                     >
                       <User className="w-5 h-5 mr-2" />
                       {user.email || 'Guest'}
-};
+                               </Button>
 
 export default Navbar;

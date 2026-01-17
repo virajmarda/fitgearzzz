0855a37d8c67c121e442e594679064183af4dde4
virajmarda <sheetalmarda123@gmail.com>
2026-01-17 12:59:02 +0530

    Fix incorrect Shopify domain in Buy Button component

--- frontend/src/components/ShopifyBuyButton.js
+++ frontend/src/components/ShopifyBuyButton.js
@@ -29,7 +29,7 @@ const ShopifyBuyButton = ({ productId }) => {
 
     loadShopifyBuy(() => {
       const client = window.ShopifyBuy.buildClient({
-        domain: "fitgearzzz.gnzuhe-en.myshopify.com.com", // your .myshopify.com domain
+        domain: "gnzuhe-en.myshopify.com", // your .myshopify.com domain
         storefrontAccessToken: process.env.REACT_APP_SHOPIFY_CLIENT_KEY,
       });
 

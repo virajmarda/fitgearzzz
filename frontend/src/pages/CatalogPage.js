f58d101b1414eaba03f82beb3fe11c3c8e6ce1c4
virajmarda <sheetalmarda123@gmail.com>
2026-01-17 11:57:18 +0530

    Add all 4 products to CatalogPage with correct IDs

--- frontend/src/pages/CatalogPage.js
+++ frontend/src/pages/CatalogPage.js
@@ -3,12 +3,24 @@ import ShopifyBuyButton from "../components/ShopifyBuyButton";
 
 function CatalogPage() {
   return (
-    <div>
-      <h2>Catalog</h2>
+    <div className="max-w-7xl mx-auto py-10 px-4">
+      <h2 className="text-3xl font-bold text-white mb-8">
+        Shop All Products
+      </h2>
 
-      <ShopifyBuyButton productId="9078870606050" />
-      <ShopifyBuyButton productId="PROD_ID_2" />
-      <ShopifyBuyButton productId="PROD_ID_3" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {/* Adjustable Waist Trimmer Belt */}
+        <ShopifyBuyButton productId="9078870606050" />
+
+        {/* 150Lbs Resistance Bands Set */}
+        <ShopifyBuyButton productId="9078771810530" />
+
+        {/* Adjustable Counting Grip */}
+        <ShopifyBuyButton productId="9077798306018" />
+
+        {/* Knee Patches */}
+        <ShopifyBuyButton productId="9064695922914" />
+      </div>
     </div>
   );
 }

4cba7db2ad885339d57bbca264aeb04b88d66274
virajmarda <sheetalmarda123@gmail.com>
2026-05-13 17:02:58 +0530

    Add Story route and import to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,7 @@ import AuthCallback from "./pages/AuthCallback";
 import CustomerLogout from "./pages/CustomerLogout";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
+import Story from "./pages/Story";
 import Wishlist from './pages/Wishlist';
 import Contact from "./pages/Contact";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
@@ -85,6 +86,7 @@ const AppContent = () => {
           <Route path="/products/:handle" element={<ProductDetail />} />
           <Route path="/about" element={<About />} />
           <Route path="/blog" element={<Blog />} />
+                <Route path="/story" element={<Story />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="/terms" element={<TermsOfService />} />
